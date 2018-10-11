694116b90b93d326e1353f1100078c90f60e17d0
Shaun McArthur <shamca@bell.net>
2018-10-11 16:45:25 -0400

    fixed schema - data mismatch when inserting user rows

---
+++ app/models/test_user.rb
@@ -0,0 +1,3 @@
+class TestUser < User
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 2018_10_11_191655) do
 
   create_table "customers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "fname"
-    t.string "lname"
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -27,8 +27,8 @@ ActiveRecord::Schema.define(version: 2018_10_11_191655) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "fname"
-    t.string "lname"
+    t.string "first_name"
+    t.string "last_name"
     t.string "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -42,8 +42,6 @@ ActiveRecord::Schema.define(version: 2018_10_11_191655) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.string "first_name"
-    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
