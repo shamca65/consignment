e6f6b097a3b191a07d0b4b53e6961d0388435c40
Shaun McArthur <shamca@bell.net>
2018-10-11 12:43:12 -0400

    added static homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :items
   resources :customers
+
+  root to: 'static#homepage'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
