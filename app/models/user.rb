f43c36dad39d9da0b27c05b65067b5da612b39fe
Shaun McArthur <shamca@bell.net>
2018-10-17 13:21:16 -0400

    integrated new account form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  #
   devise :database_authenticatable, :rememberable
 
   validates_presence_of :first_name, :last_name, :phone
