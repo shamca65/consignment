c5d639356f9f3a2661db8c3916e377edcda0a203
Shaun McArthur <shamca@bell.net>
2018-11-08 13:16:35 -0500

    re-arranged assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui
 //= require bootstrap-sprockets
 //= require gritter
 //= require turbolinks
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,8 +14,6 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 # Rails.application.config.assets.precompile += %w( jquery.js )
 #
-Rails.application.config.assets.precompile += %w( jquery.gritter.js )
-Rails.application.config.assets.precompile += %w( jquery.gritter.css )
 
 
 
