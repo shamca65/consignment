2f4be40afbefc7dd052533cc3ce5f68569cb51ab
Shaun McArthur <shamca65@gmail.com>
2018-11-19 15:08:30 -0500

    added take-in search page (item/new)

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,5 +1,48 @@
 $( document ).ready(function() {
-    if(!page.isControllerAndAction('items', 'edit')) return;
-    alert('ok this is ok')
-    console.log( "ready!" );
+
+    var contr = page.controller()
+    var act = page.action()
+
+    switch(contr) {
+        case "items":
+            switch(act) {
+                case "edit":
+                    $("#currencyInput").jqxNumberInput({
+                        template: 'primary',
+                        width: '150px',
+                        height: '35px',
+                        symbol: '$',
+                        spinButtons: true,
+                        digits: 3,
+                        decimalDigits: 2
+                        });
+
+                    break;
+                default:
+                    break;
+            }
+            break;
+        case "customers":
+            switch(act) {
+                case "new":
+                    $("#phoneInput").jqxMaskedInput({ mask: '(###)###-####', width: 250, height: 30  });
+
+
+                    $('#phoneInput').jqxValidator({
+                        rules: [
+                            { input: '#phoneInput', message: 'Invalid phone number!', action: 'valuechanged, blur', rule: 'phone' }]
+                    });
+
+                    $('#emailInput').jqxValidator({
+                        rules: [
+                            { input: '#emailInput', message: 'E-mail is required!', action: 'keyup, blur', rule: 'required' },
+                            { input: '#emailInput', message: 'Invalid e-mail!', action: 'keyup', rule: 'email' }]
+                    });
+
+            }
+            break;
+        case "static":
+            break;
+        default:
+    }
 });
