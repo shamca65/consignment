1532bdd72a50f397cc7d9aeb760026925febe509
Shaun McArthur <shamca@bell.net>
2018-12-09 19:51:29 -0500

    cleaned up some divs n shit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,9 @@
 //= require_tree .
 
 $(document).ready(function(){
+    var contr = page.controller();
+    var act = page.action();
+
     Paloma.start();
 
     $('#js-ajax-link').on('click', function(e){
@@ -25,5 +28,54 @@ $(document).ready(function(){
             Paloma.start();
         });
     });
+
+
+
+    switch(contr) {
+        case "items":
+            switch(act) {
+                case "edit":
+                    $("#currencyInput").jqxNumberInput({
+                        template: 'primary',
+                        width: '150px',
+                        height: '35px',
+                        symbol: '$',
+                        spinButtons: true,
+                        digits: 3,
+                        decimalDigits: 2
+                    });
+
+                    break;
+                default:
+                    break;
+            }
+            break;
+        case "customers":
+            switch(act) {
+                case "new":
+                    $("#phoneInput").jqxMaskedInput({ mask: '(###)###-####', width: 250, height: 30  });
+
+
+                    $('#phoneInput').jqxValidator({
+                        rules: [
+                            { input: '#phoneInput', message: 'Invalid phone number!', action: 'valuechanged, blur', rule: 'phone' }]
+                    });
+
+                    $('#emailInput').jqxValidator({
+                        rules: [
+                            { input: '#emailInput', message: 'E-mail is required!', action: 'keyup, blur', rule: 'required' },
+                            { input: '#emailInput', message: 'Invalid e-mail!', action: 'keyup', rule: 'email' }]
+                    });
+
+                case "takein":
+
+                    break;
+
+            }
+            break;
+        case "static":
+            break;
+        default:
+    }
 
 });
