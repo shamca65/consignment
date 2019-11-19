222c536b4e7574d8804d9d143c897544df0bb7a0
Shaun McArthur <shamca@bell.net>
2019-11-19 14:48:27 -0500

    cleaning up

--- Javascript_cd/chap8/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/csh -f
-
-/export/netscape/LiveWire/bin/lwcomp -v -o contact.web *.html *.js
-
-chmod 644 contact.web
--- test/factories/configs.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :config do
-    
-  end
-end
